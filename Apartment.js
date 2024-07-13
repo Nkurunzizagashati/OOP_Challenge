ef54ec0628a72551c55a0cb43ae8f4131ef57e23
Fabrice <f.nkurunziz@alustudent.com>
2024-07-13 12:30:00 +0200

    apartment management system

---
+++ Apartment.js
@@ -0,0 +1,20 @@
+/**
+ * Represents an apartment.
+ */
+class Apartment {
+  /**
+   * Creates a new apartment.
+   * @param {string} name - The name of the apartment.
+   * @param {string} address - The address of the apartment.
+   * @param {number} numberOfBedrooms - The number of bedrooms in the apartment.
+   * @param {boolean} isOccupied - Whether the apartment is currently occupied.
+   */
+  constructor(name, address, numberOfBedrooms, isOccupied) {
+    this.name = name;
+    this.address = address;
+    this.numberOfBedrooms = numberOfBedrooms;
+    this.isOccupied = isOccupied;
+  }
+}
+
+module.exports = Apartment;
